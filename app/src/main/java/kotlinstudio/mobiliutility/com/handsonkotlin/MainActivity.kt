a6d33d1eae64937d70604d3c7208826aa788d5fe
gaikwadchetan93 <gaikwadchetan93@gmail.com>
2018-06-12 14:31:04 +0530

    Firebase connected

--- app/src/main/java/kotlinstudio/mobiliutility/com/handsonkotlin/MainActivity.kt
+++ app/src/main/java/kotlinstudio/mobiliutility/com/handsonkotlin/MainActivity.kt
@@ -3,12 +3,12 @@ package kotlinstudio.mobiliutility.com.handsonkotlin
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinstudio.mobiliutility.com.handsonkotlin.databinding.ActivityMainBinding
 import kotlinstudio.mobiliutility.com.handsonkotlin.model.User
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-    //lateinit var mainBinding : ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
